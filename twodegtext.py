5bc14b3e094657c3c0cf64c5083cd37b2e984a69
XFH <apple94079@gmail.com>
2020-05-17 14:22:33 +0800

    準備5/18的報告

--- twodegtext.py
+++ twodegtext.py
@@ -4,7 +4,7 @@ import calculusMathod as cm
 import openpyxl
 
 
-def main(length = 200, width = 50, energyMaxmum = 20, name = "length=200, width=50 system.xlsx"):
+def main(length = 200, width = 100, energyMaxmum = 20, name = "length=200, width=100 system.xlsx"):
 
     workbook = openpyxl.Workbook()
 
@@ -16,34 +16,34 @@ def main(length = 200, width = 50, energyMaxmum = 20, name = "length=200, width=
     system.attach_lead(lead.reversed())
 
     system = system.finalized()
-    """
-    kwant.plot(system)
 
+    kwant.plot(system)
+    """
     energies, densities = cm.getDOS(system)
     DosSheet = workbook.create_sheet("DOS")
     DosSheet.append(energy for energy in energies)
     DosSheet.append(densitiy for densitiy in densities)
     print("OK")
-
+    """
 
     momentum = 6.28
     momenta, band = cm.getBandStructure(lead.finalized(), momentum, length)
-    BandSheet = workbook.create_sheet("Band")
+    BandSheet = workbook.create_sheet()
     BandSheet.append(momenta)
     for i in range(length):
         BandSheet.append(band[i])
     print("OK")
 
-
+    """
     energies, data = cm.getConductance(system, energyMaxmum)
     ConductanceSheet = workbook.create_sheet("Conductance")
     ConductanceSheet.append(energies)
     ConductanceSheet.append(data)
     print("OK")
-
-    workbook.save(name)
     """
-    cm.computeEigenvalues(system)
+    workbook.save(name)
+
+    #cm.computeEigenvalues(system)
 
 if __name__ == '__main__':
     main()
